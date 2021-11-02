0207df9c3aad6b804cf643e2f9ad91554bb01814
Martynov1814 <martynov_mk@mail.ru>
2021-11-02 18:23:11 +0300

    Add over files

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+define ('SITE_NAME', "Cut your URL");
+define ('HOST', "http://" . $_SERVER['HTTP_HOST']);
+
+define('DB_HOST', '127.0.0.1'); //Или glo-academy-curse.ru
+define('DB_NAME', 'cut_url');
+define('DB_USER', 'mysql');
+define('DB_PASS', 'mysql');
+
